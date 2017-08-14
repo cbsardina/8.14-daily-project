4a9c93dad9526ef8c0bfa04c4506096cfbc6bcd1
Christopher Sardina <chrissardina@Chris-Sardina.local>
2017-08-14 17:53:35 -0400

    8.14: DP Node Date Facts: Complete and tested.

--- dateFacts.js
+++ dateFacts.js
@@ -1,10 +1,30 @@
 const moment = require('moment');
 const chalk = require('chalk');
 
-let now = moment().format("dddd, MMMM Do YYYY, h:mm:ss a"); // "Sunday, February 14th 2010, 3:25:50 pm"
+let now = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 let dayOfYear = moment().dayOfYear();
-// console.log(moment().format(););
+let hours = moment().hours();
+let minutes = moment().minutes();
+let seconds = moment().seconds();
+let dst = moment().isDST();
+let leap = moment().isLeapYear();
 
-console.log(now);
-console.log(dayOfYear);
-console.log(chalk.blue('Hello world!'));
+function secondsInDay () {
+  return chalk.blueBright((hours * 60 * 60) + (minutes * 60) + seconds);
+}
+function isDst () {
+  if (dst) {
+    return chalk.green('is');
+  } else { return chalk.red(' is not '); }
+}
+function isLeap () {
+  if (leap) {
+    return chalk.green(leap);
+  } else { return chalk.red(' is not '); }
+}
+
+console.log('It is ' + chalk.blue(now) + '.');
+console.log('It is the ' + chalk.magenta(dayOfYear + 'th') + ' day of the year.');
+console.log('It is ' + secondsInDay() + ' seconds into the day.');
+console.log('It ' + isDst() + ' during Daylight Saving Time.');
+console.log('It ' + isLeap() + ' not a leap year.');
