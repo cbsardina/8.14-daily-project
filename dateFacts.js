4ce407f0b4fdc6bfa72e3ef0019b6f89bdb2e7fc
Christopher Sardina <chrissardina@Chris-Sardina.local>
2017-08-14 18:46:19 -0400

    8.14: DP Node Date Facts: Complete

--- dateFacts.js
+++ dateFacts.js
@@ -1,30 +1,30 @@
 const moment = require('moment');
 const chalk = require('chalk');
 
-let now = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-let dayOfYear = moment().dayOfYear();
-let hours = moment().hours();
-let minutes = moment().minutes();
-let seconds = moment().seconds();
-let dst = moment().isDST();
-let leap = moment().isLeapYear();
+const now = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+const dayOfYear = moment().dayOfYear();
+const hours = moment().hours();
+const minutes = moment().minutes();
+const seconds = moment().seconds();
+const dst = moment().isDST();
+const leap = moment().isLeapYear();
 
 function secondsInDay () {
-  return chalk.blueBright((hours * 60 * 60) + (minutes * 60) + seconds);
+  return chalk.cyanBright.bold((hours * 60 * 60) + (minutes * 60) + seconds);
 }
 function isDst () {
   if (dst) {
-    return chalk.green('is');
-  } else { return chalk.red(' is not '); }
+    return chalk.greenBright.bold('is');
+  } else { return chalk.red.bold('is not'); }
 }
 function isLeap () {
   if (leap) {
-    return chalk.green(leap);
-  } else { return chalk.red(' is not '); }
+    return chalk.greenBright.bold('is');
+  } else { return chalk.red.bold('is not'); }
 }
 
-console.log('It is ' + chalk.blue(now) + '.');
-console.log('It is the ' + chalk.magenta(dayOfYear + 'th') + ' day of the year.');
+console.log('It is ' + chalk.blueBright.bold(now) + '.');
+console.log('It is the ' + chalk.magenta.bold(dayOfYear + 'th') + ' day of the year.');
 console.log('It is ' + secondsInDay() + ' seconds into the day.');
 console.log('It ' + isDst() + ' during Daylight Saving Time.');
-console.log('It ' + isLeap() + ' not a leap year.');
+console.log('It ' + isLeap() + ' a leap year.');
